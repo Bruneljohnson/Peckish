c2faade1f9f36676f17c878f014fe1c7b7e08503
Bruneljohnson <photography@bruneljohnson.com>
2022-01-20 02:43:52 +0000

    Updates

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import classes from "./Card.module.css";
-
-const Card = (props) => {
-  const classSet = `${classes.card} ${props.className}`;
-
-  return <div className={classSet}>{props.children}</div>;
-};
-export default Card;
--- src/helpers/Helpers.js
+++
@@ -1,7 +0,0 @@
-export const SET_TIMEOUT = (sec) => {
-  return new Promise(function (_, reject) {
-    setTimeout(() => {
-      reject(new Error(`Request Failed! Please try again.`));
-    }, sec * 1000);
-  });
-};
